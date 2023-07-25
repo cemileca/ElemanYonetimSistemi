a4d9c2f81f58a1092097494b7ac766a526519b34
Güzel İşler <97739544+cemileca@users.noreply.github.com>
2023-07-25 07:04:14 +0300

    25.07.2023 Tasarımsal

--- ElemanYonetimSistemi/Presentation/ElemanYonetimSistemi.WinForm/ElemanYonetimSistemiAnaForm.Designer.cs
+++ ElemanYonetimSistemi/Presentation/ElemanYonetimSistemi.WinForm/ElemanYonetimSistemiAnaForm.Designer.cs
@@ -34,68 +34,209 @@
             button4 = new Button();
             button5 = new Button();
             button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
             SuspendLayout();
             // 
             // button1
             // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             button1.Location = new Point(12, 12);
             button1.Name = "button1";
-            button1.Size = new Size(207, 191);
+            button1.Size = new Size(179, 152);
             button1.TabIndex = 0;
             button1.Text = "Yapılacaklar";
             button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            button2.Location = new Point(243, 12);
+            button2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button2.Location = new Point(240, 12);
             button2.Name = "button2";
-            button2.Size = new Size(207, 191);
+            button2.Size = new Size(179, 152);
             button2.TabIndex = 0;
             button2.Text = "Satışlar";
             button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            button3.Location = new Point(469, 12);
+            button3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button3.Location = new Point(468, 12);
             button3.Name = "button3";
-            button3.Size = new Size(207, 191);
+            button3.Size = new Size(179, 152);
             button3.TabIndex = 0;
             button3.Text = "Elemanlar";
             button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            button4.Location = new Point(12, 224);
+            button4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button4.Location = new Point(12, 216);
             button4.Name = "button4";
-            button4.Size = new Size(207, 191);
+            button4.Size = new Size(179, 152);
             button4.TabIndex = 0;
             button4.Text = "Muhasebe";
             button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            button5.Location = new Point(243, 224);
+            button5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button5.Location = new Point(240, 216);
             button5.Name = "button5";
-            button5.Size = new Size(207, 191);
+            button5.Size = new Size(179, 152);
             button5.TabIndex = 0;
             button5.Text = "Tedarikçiler";
             button5.UseVisualStyleBackColor = true;
             // 
             // button6
             // 
-            button6.Location = new Point(469, 224);
+            button6.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button6.Location = new Point(468, 216);
             button6.Name = "button6";
-            button6.Size = new Size(207, 191);
+            button6.Size = new Size(179, 152);
             button6.TabIndex = 0;
             button6.Text = "Müşteriler";
             button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button7.Location = new Point(469, 420);
+            button7.Name = "button7";
+            button7.Size = new Size(179, 152);
+            button7.TabIndex = 0;
+            button7.Text = "Diğer";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button8.Location = new Point(243, 420);
+            button8.Name = "button8";
+            button8.Size = new Size(179, 152);
+            button8.TabIndex = 0;
+            button8.Text = "Ileri Zaman Planlaması";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button9.Location = new Point(12, 420);
+            button9.Name = "button9";
+            button9.Size = new Size(179, 152);
+            button9.TabIndex = 0;
+            button9.Text = "Raporlar";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(711, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(115, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Sisteme Giriş Yapıldı:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(711, 43);
+            label2.Name = "label2";
+            label2.Size = new Size(40, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Adınız";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(711, 67);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 15);
+            label3.TabIndex = 1;
+            label3.Text = "Soyadınız";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(711, 91);
+            label4.Name = "label4";
+            label4.Size = new Size(47, 15);
+            label4.TabIndex = 1;
+            label4.Text = "Yetkiniz";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(711, 115);
+            label5.Name = "label5";
+            label5.Size = new Size(59, 15);
+            label5.TabIndex = 1;
+            label5.Text = "Ünvanınız";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(711, 139);
+            label6.Name = "label6";
+            label6.Size = new Size(66, 15);
+            label6.TabIndex = 1;
+            label6.Text = "Departman";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(711, 163);
+            label7.Name = "label7";
+            label7.Size = new Size(84, 15);
+            label7.TabIndex = 1;
+            label7.Text = "Alt Departman";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(711, 187);
+            label8.Name = "label8";
+            label8.Size = new Size(53, 15);
+            label8.TabIndex = 1;
+            label8.Text = "Maaşınız";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(711, 211);
+            label9.Name = "label9";
+            label9.Size = new Size(52, 15);
+            label9.TabIndex = 1;
+            label9.Text = "Puanınız";
             // 
             // ElemanYonetimSistemiAnaForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(979, 653);
+            ClientSize = new Size(951, 614);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(button4);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
             Controls.Add(button6);
             Controls.Add(button3);
             Controls.Add(button5);
@@ -105,6 +246,7 @@
             Name = "ElemanYonetimSistemiAnaForm";
             Text = "Ana Yönetim Paneli";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -115,5 +257,17 @@
         private Button button4;
         private Button button5;
         private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
     }
 }
